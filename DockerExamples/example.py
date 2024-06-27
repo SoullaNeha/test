50305cad3e4845bb1ec6fa696cfc89f80246c492
SoullaNeha <160470650+SoullaNeha@users.noreply.github.com>
2024-06-27 14:29:28 +0530

    Update app.py

--- DockerExamples/example.py
+++ DockerExamples/example.py
@@ -1,3 +1,10 @@
-#!/bin/bash
+from flask import Flask
 
-echo "hello=====world"
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0', port=8000)
